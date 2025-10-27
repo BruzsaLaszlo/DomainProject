42164e421dae159e416175d6119db0739ece8987
Bruzsa László <bruzsalaci@gmail.com>
2025-10-27 22:19:30 +0100

    add field: String ipAddress

--- CloudflareDnsManagement/src/test/java/bruzsal/dnsmanagement/service/DnsRecordControllerIT.java
+++ CloudflareDnsManagement/src/test/java/bruzsal/dnsmanagement/service/DnsRecordControllerIT.java
@@ -1,9 +1,15 @@
 package bruzsal.dnsmanagement.service;
 
 import bruzsal.dnsmanagement.TestSessionConfig;
+import bruzsal.dnsmanagement.controller.request.DDnsCommand;
 import bruzsal.dnsmanagement.controller.request.DnsRecordCommand;
-import bruzsal.dnsmanagement.dto.model.DnsRecordDto;
-import org.junit.jupiter.api.*;
+import bruzsal.dnsmanagement.controller.request.IpAddressType;
+import bruzsal.dnsmanagement.dto.DnsRecordDto;
+import bruzsal.dnsmanagement.service.alt.IpAddress;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +21,10 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import java.util.List;
 import java.util.Random;
 
+import static bruzsal.dnsmanagement.controller.request.IpAddressType.IPV4;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestSessionConfig.class)
@@ -58,7 +66,7 @@ class DnsRecordControllerIT {
                 .expectBody(DnsRecordDto.class)
                 .returnResult()
                 .getResponseBody();
-        Assertions.assertNotNull(dnsRecordDto);
+        assertNotNull(dnsRecordDto);
         dnsRecordId = dnsRecordDto.id();
     }
 
@@ -88,7 +96,8 @@ class DnsRecordControllerIT {
                 .uri("/api/records/filterByNameStartsWith?prefix=test")
                 .exchange()
                 .expectBodyList(DnsRecordDto.class)
-                .returnResult().getResponseBody();
+                .returnResult()
+                .getResponseBody();
         assertThat(list).isNotEmpty();
         list.forEach(dnsRecord ->
                 webTestClient
@@ -96,6 +105,29 @@ class DnsRecordControllerIT {
                         .uri("/api/records/{id}", dnsRecord.id())
                         .exchange()
                         .expectStatus().isOk());
+    }
+
+    @Test
+    void updateDns(@Autowired IpAddress ipAddress) {
+        IpAddressType ipAddressType = IPV4;
+        String name = "test.ddns.laci.my";
+        String ip = ipAddress.get(ipAddressType);
+        DDnsCommand dDnsCommand = new DDnsCommand(ipAddressType, name, ip);
+
+        DnsRecordDto dnsRecordDto = webTestClient
+                .patch()
+                .uri("/api/records/ddns")
+                .bodyValue(dDnsCommand)
+                .exchange()
+                .expectStatus().isAccepted()
+                .expectBody(DnsRecordDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertNotNull(dnsRecordDto);
+        assertEquals(ipAddressType.value, dnsRecordDto.type());
+        assertEquals(name, dnsRecordDto.name());
+        assertEquals(ip, dnsRecordDto.content());
     }
 
 }
